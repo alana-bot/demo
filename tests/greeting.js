9f3f0d46c05b4ac80e69c6166cc447ca92364a84
Adam Juhasz <hello@ajuhasz.io>
2017-03-13 18:33:32 -0400

    Delete greeting.js

--- tests/greeting.js
+++
@@ -1,9 +0,0 @@
-describe('greeting', function() {
-  it('basic', function() {
-    const prom = newTest()
-      .expectText('Welome. I\'m a demo for botler')
-      .expectText('What\'s your name?')
-      .run()
-    return prom;
-  });
-});
